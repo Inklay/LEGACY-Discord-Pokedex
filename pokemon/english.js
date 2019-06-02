0336ae41fb088e51fd0c7e70ef08afbcd38f9c5b
Inklay <40170684+Inklay@users.noreply.github.com>
2019-06-02 17:53:25 +0200

    Added english height and weith ans support for mega evolution and alolan form

--- pokemon/english.js
+++ pokemon/english.js
@@ -78,9 +78,9 @@ module.exports = {
             });
             if (!found) {
                 if (alola)
-                    channel.sendMessage("Can not find \"Alolan " + content.substring(8).charAt(0).toUpperCase() + content.substring(8).slice(1) + "\".");
+                    channel.sendMessage("Can not find \"Alolan " + search.substring(40).charAt(0).toUpperCase() + search.substring(40).slice(1) + "\".");
                 else
-                    channel.sendMessage("Can not find \"" + content.substring(8).charAt(0).toUpperCase() + content.substring(8).slice(1) + "\".");
+                    channel.sendMessage("Can not find \"" + search.substring(40).charAt(0).toUpperCase() + search.substring(40).slice(1) + "\".");
                 return;
             }
             if (alola)
@@ -91,21 +91,80 @@ module.exports = {
                 if ($(this)[0].attribs.href == "/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number")
                     number = $(this)[0].children[0].children[0].data.substring(1);
             });
-            $('tr > td > a', body).each(function(i) {
+            found = 0;
+            $('tr > td > a', body).each(function() {
+                if (alola && $(this)[0].attribs.class != null && $(this)[0].attribs.class == "image" && $(this)[0].attribs.title != null && $(this)[0].attribs.title.search("Alolan") != -1)
+                    found = 1;
+                else if (is_mega && $(this)[0].attribs.class != null && $(this)[0].attribs.class == "image" && $(this)[0].attribs.title != null && $(this)[0].attribs.title.search("Mega") != -1)
+                    found = 1;
+            });
+            if (!found) {
+                if (alola) {
+                    channel.sendMessage("\"" + search.substring(40).charAt(0).toUpperCase() + search.substring(40).slice(1) + "\" Does not have an alolan form.");
+                    return;
+                } else if (is_mega) {
+                    channel.sendMessage("\"" + search.substring(40).charAt(0).toUpperCase() + search.substring(40).slice(1) + "\" Does not have a mega evolution.");
+                    return;
+                } 
+            }
+            $('tr > td > a', body).each(function() {
                 if ($(this)[0].attribs.href != null && $(this)[0].attribs.href.search("(type)") != -1) {
-                    if (type1 == null) {
+                    if (alola && type1 == null) {
+                        if ($(this).parent().parent().parent().parent().next()[0].type == "tag" && $(this).parent().parent().parent().parent().next()[0].name == "small"
+                        && $(this).parent().parent().parent().parent().next()[0].children[0].data.search("Alolan") != -1) {
+                            type1 = $(this)[0].attribs.href.substring(6, ($(this)[0].attribs.href.length - 7));
+                            type2 = $(this).parent().next().children()[0].attribs.href.substring(6, ($(this).parent().next().children()[0].attribs.href.length - 7));
+                        }
+                    } else if (is_mega && type1 == null) {
+                        if ($(this).parent().parent().parent().parent().next()[0].type == "tag" && $(this).parent().parent().parent().parent().next()[0].name == "small"
+                        && $(this).parent().parent().parent().parent().next()[0].children[0].data.search("Mega") != -1) {
+                            type1 = $(this)[0].attribs.href.substring(6, ($(this)[0].attribs.href.length - 7));
+                            type2 = $(this).parent().next().children()[0].attribs.href.substring(6, ($(this).parent().next().children()[0].attribs.href.length - 7));
+                        }
+                    } else if (type1 == null) {
                         type1 = $(this)[0].attribs.href.substring(6, ($(this)[0].attribs.href.length - 7));
                         type2 = $(this).parent().next().children()[0].attribs.href.substring(6, ($(this).parent().next().children()[0].attribs.href.length - 7));
                     }
+                } else if ($(this)[0].attribs.href != null && $(this)[0].attribs.href == "/wiki/Pok%C3%A9mon_category") {
+                    if ($(this)[0].children[0].children[0].type == "text")
+                        family = $(this)[0].children[0].children[0].data;
+                    else {
+                        family = $(this)[0].children[0].children[0].children[0].data;
+                        family += " Pokémon";
+                    }
                 }
+            });
+            $('tr > td > b > a', body).each(function() {
+                if ($(this)[0].attribs.href != null && $(this)[0].attribs.href == "/wiki/List_of_Pok%C3%A9mon_by_height"){
+                    color = parseInt("0x" + $(this).parent().parent()[0].attribs.style.substring(12));
+                    if (is_mega || alola) {
+                        height = $(this).parent().next()[0].children[1].children[4].children[1].children[0].data;
+                        height = height.substring(0, height.length - 1) + "/";
+                        height = height.concat($(this).parent().next()[0].children[1].children[4].children[3].children[0].data.substring(1));
+                    } else {
+                        height = $(this).parent().next()[0].children[1].children[0].children[1].children[0].data;
+                        height = height.substring(0, height.length - 1) + "/";
+                        height = height.concat($(this).parent().next()[0].children[1].children[0].children[3].children[0].data.substring(1));
+                    }
+                } else if ($(this)[0].attribs.href != null && $(this)[0].attribs.href == "/wiki/List_of_Pok%C3%A9mon_by_weight"){
+                    if (is_mega || alola) {
+                        weight = $(this).parent().next()[0].children[1].children[4].children[1].children[0].data;
+                        weight = weight.substring(0, weight.length - 1) + "/";
+                        weight = weight.concat($(this).parent().next()[0].children[1].children[4].children[3].children[0].data.substring(1));
+                    } else {
+                        weight = $(this).parent().next()[0].children[1].children[0].children[1].children[0].data;
+                        weight = weight.substring(0, weight.length - 1) + "/";
+                        weight = weight.concat($(this).parent().next()[0].children[1].children[0].children[3].children[0].data.substring(1));
+                    }
+                } 
             });
             description = "Pokédex number: " + number + "\n";
             if (type2 == "Unknown")
                 description += "Type: " + type1.charAt(0).toUpperCase() + type1.slice(1) + "\n";
             else
                 description += "Types: " + type1.charAt(0).toUpperCase() + type1.slice(1) + ", " + type2.charAt(0).toUpperCase() + type2.slice(1) + "\n";
-            /*description += "Family: " + family + "Taille: " + height + "Poids: " + weight;
-            if (ability2 == "NULL")
+            description += "Category: " + family + "\nHeight: " + height + "Weight: " + weight;
+            /*if (ability2 == "NULL")
                 description += "Ability: " + ability1 + "\n";
             else {
                 if (ability3 == "NULL")
