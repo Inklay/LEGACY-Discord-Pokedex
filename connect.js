0caf4b788f30964693189d8405df1abd8a7cd6ec
Inklay <40170684+Inklay@users.noreply.github.com>
2019-07-20 22:15:04 +0200

    Added error message if connection fail

--- connect.js
+++ connect.js
@@ -2,5 +2,9 @@ module.exports = {
     connect: function(client)
     {
         client.connect({ token: "[Enter token here]" });
+        if (!client.connected) {
+            console.log("Can not connect to Discord, please verify your token and check Discord's server statues.");
+            process.exit(1);
+        }
     }
 };
