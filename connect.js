70906d52f0a78f732aea879be5e66b2042756d49
Inklay <40170684+Inklay@users.noreply.github.com>
2019-07-31 01:54:56 +0200

    Added npm start command

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 module.exports = {
     connect: function(client)
     {
-        client.connect({ token: "[Enter token here]" });
+        client.connect({ token: "NTE4ODg3NjA2OTg1OTQ5MTg2.XTN4aQ.-b09puFaD-uInBKr50aSe8Uy_aE" });
         client.Dispatcher.on("DISCONNECTED", e => {
             console.log("Can not connect to Discord, please verify your token and check Discord's server statues.");
             process.exit(1);
