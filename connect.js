6262d6975eb12bb72c0082c6e9e8f148884bd764
Kylian Lavergne <40170684+Inklay@users.noreply.github.com>
2019-08-13 07:59:56 +0200

    Fixed auto reconnect

--- connect.js
+++ connect.js
@@ -4,7 +4,6 @@ module.exports = {
         client.connect({ token: "process.env.BOT_TOKEN" });
         client.Dispatcher.on("DISCONNECTED", e => {
             console.log("Can not connect to Discord, please verify your token and check Discord's server statues.");
-            process.exit(1);
         });
     }
 };
