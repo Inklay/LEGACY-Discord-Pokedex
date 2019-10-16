3b1f8f3dde2ff1f09ffef9e483ad7323ded1f598
Inklay <inklay@outlook.fr>
2019-10-16 12:14:53 +0200

    Fixed process env

--- src/connect.js
+++ src/connect.js
@@ -2,7 +2,7 @@ module.exports = {
     connect: function(client)
     {
         console.log("connect.js: connecting");
-        client.connect({ token: "process.env.BOT_TOKEN" });
+        client.connect({ token: process.env.BOT_TOKEN });
         client.Dispatcher.on("DISCONNECTED", e => {
             console.log("connect.js: can not connect to Discord, please verify your token and check Discord's server statues.");
         });
